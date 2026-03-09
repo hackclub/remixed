c0f6a093a8c87f24b1053c1ba0d93c61be0c644b
Shuflduf <shuflduf@gmail.com>
2026-03-08 20:09:34 -0600

    feat(landing): adding text and sleeve to records

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
-	let records = $state(['RHYTHM GAMES', 'THE SECOND ONE', 'THE THIRd ONE']);
+	let records = $state(['RHYTHM GAMES', 'THE SECOND ONE', 'THE THIRd ONE', 'fourth']);
 	console.log('FUCK');
 </script>
 
-<div class="flex w-screen justify-center mt-16 relative">
+<div
+	class="relative mt-16 flex w-screen justify-center"
+>
 	{#each records as record, i}
 		<div
-			class="absolute h-[400px] w-[400px] rounded-full border border-blue-500 bg-red-500"
+			class="absolute w-200 h-200 rounded-full border border-blue-500 bg-red-500 flex items-start justify-center pt-4"
 			style="top: {i * 60}px; z-index: {i}"
 		>
+			<h1 class="text-white">{record}</h1>
 		</div>
 	{/each}
+
+	<div
+		class="absolute w-200 bg-stone-800 rounded-b-sm z-50 h-200 top-60"
+	></div>
 </div>
