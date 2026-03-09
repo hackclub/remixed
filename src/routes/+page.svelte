29133d17a46a29ffb3afeb293a149b38383ae9bf
Shuflduf <shuflduf@gmail.com>
2026-03-08 19:55:20 -0600

    feat(landing): started implementing records

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	let records = $state(['RHYTHM GAMES', 'THE SECOND ONE', 'THE THIRd ONE']);
+	console.log('FUCK');
+</script>
+
+<div class="flex w-screen justify-center mt-16 relative">
+	{#each records as record, i}
+		<div
+			class="absolute h-[400px] w-[400px] rounded-full border border-blue-500 bg-red-500"
+			style="top: {i * 60}px; z-index: {i}"
+		>
+		</div>
+	{/each}
+</div>
