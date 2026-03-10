3daf2a46665e92f497876c112d3af8e46c565c1b
Shuflduf <shuflduf@gmail.com>
2026-03-09 19:01:05 -0600

    feat(ui): add cassetts to projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,7 +2,7 @@
 	import Sidebar from '$lib/Sidebar.svelte';
 	import { onMount } from 'svelte';
 
-	let userProjects = $state(null);
+	let userProjects = $state([]);
 
 	onMount(() => {
 		try {
@@ -22,12 +22,29 @@
 </script>
 
 <Sidebar />
-<div class="ml-40">
-	<h1>LIST YOUR PROJECTS</h1>
-	<a href="/projects/new">Create New</a>
-	{#if userProjects}
-		<pre>
-			<p>{JSON.stringify(userProjects, null, 2)}</p>
-		</pre>
-	{/if}
+<div class="ml-40 p-8">
+	<div class="mb-6 flex items-center gap-4">
+		<h1 class="text-2xl font-bold">Your Projects</h1>
+		<a href="/projects/new">New</a>
+	</div>
+
+	<div class="flex flex-wrap gap-6">
+		{#each userProjects as project}
+			<div class="relative">
+				<img src="/cassette.png" alt="cassette" />
+				<div
+					class="absolute top-10 left-1/2 flex h-14 w-80 -translate-x-1/2 items-center justify-center"
+				>
+					<h1 class="line-clamp-2 text-center font-gothic text-2xl leading-tight">
+						{project.title}
+					</h1>
+				</div>
+				<p
+					class="absolute top-46 left-1/2 line-clamp-2 w-84 -translate-x-1/2 font-zcool text-xs leading-5"
+				>
+					{project.description}
+				</p>
+			</div>
+		{/each}
+	</div>
 </div>
