8f56f1f6b72520a60beebf91c5be0d1aaab4d78f
Shuflduf <shuflduf@gmail.com>
2026-03-12 23:19:36 -0600

    feat(ui): make the actual project page based on a cd. the colours are actual ass rn but idc

--- src/routes/projects/[id]/+page.svelte
+++ src/routes/projects/[id]/+page.svelte
@@ -44,98 +44,119 @@
 
 <Sidebar />
 <div class="h-screen pt-8 pr-8 pl-40">
-	<main class="mx-auto flex h-full w-200 items-center">
+	<main class="mx-auto flex h-full w-full items-center">
 		<form method="POST" action="?/update">
-			<div class="flex w-full flex-row justify-evenly gap-8">
-				<!-- TODO: make the cover art like balatro or smth -->
-				<div>
+			<div
+				popover
+				id="hackatime-project-select"
+				class="popover fixed top-1/2 left-1/2 w-90 -translate-1/2 rounded-md bg-secondary p-4 shadow-xl"
+			>
+				<h1 class="text-center font-nikkyou text-2xl text-text">Hackatime Projects</h1>
+				<select
+					multiple
+					name="hackatimeProjects"
+					class="w-full rounded-md bg-primary p-4 text-center font-gothic font-bold text-accent ring-primary focus:ring-2 focus:outline-none"
+				>
+					{#each hackatimeProjects as proj}
+						{#if proj.claimedBy == null}
+							<option selected={proj.claimedBy == data.project!.id} value={proj.name}>
+								{proj.name}
+							</option>
+						{:else}
+							<option disabled value={proj.name}>{proj.name}</option>
+						{/if}
+					{/each}
+				</select>
+				<sub class="mt-2 block text-center font-zcool text-text">Ctrl+Click to select multiple</sub>
+			</div>
+			<div class="grid w-full grid-cols-2 gap-8 bg-black">
+				<div class="flex w-full flex-col bg-text p-4">
+					{#if editing}
+						<input
+							type="text"
+							name="title"
+							bind:value={draft.title}
+							class="mb-4 rounded-md bg-accent px-4 py-2 text-center font-nikkyou text-5xl text-primary ring-secondary outline-none focus:ring-2"
+						/>
+					{:else}
+						<h1 class="mb-4 text-center font-nikkyou text-5xl text-primary">{draft.title}</h1>
+					{/if}
+					<div class="flex justify-between">
+						{#if editing}
+							<button
+								type="button"
+								popovertarget="hackatime-project-select"
+								class="mb-2 cursor-pointer rounded-md bg-accent p-2 font-gothic text-text"
+							>
+								Hackatime
+							</button>
+							<select
+								name="category"
+								id="category"
+								bind:value={draft.category}
+								class="mb-2 cursor-pointer rounded-md bg-accent p-2 text-center font-gothic text-sm font-bold text-text ring-primary focus:ring-2 focus:outline-none"
+							>
+								<option value="GAME">Game</option>
+								<option value="WEBSITE">Website</option>
+								<option value="DESKTOP_APP">Desktop App</option>
+								<option value="CLI">CLI</option>
+								<option value="OTHER">Other</option>
+							</select>
+						{:else}
+							<span
+								class="font-gothic text-xl text-accent"
+								title={data.project!.hackatimeProjects!.join(', ')}
+							>
+								{hoursText}
+							</span>
+							<span
+								class="font-gothic text-xl text-accent"
+								title={data.project!.hackatimeProjects!.join(', ')}
+							>
+								{data.project?.category.replaceAll('_', ' ')}
+							</span>
+						{/if}
+					</div>
+
+					{#if editing}
+						<textarea
+							name="description"
+							bind:value={draft.description}
+							class="h-0 grow overflow-auto rounded-md bg-accent p-2 text-center font-zcool text-text ring-secondary outline-none focus:ring-2"
+						>
+						</textarea>
+					{:else}
+						<div class="h-0 grow overflow-auto text-accent">
+							<p class="text-center font-zcool">
+								{draft.description}
+							</p>
+						</div>
+					{/if}
+				</div>
+				<div class="relative">
 					<img
 						src={draft?.coverArt ?? '/404.jpg'}
 						onerror={(e: any) => (e.currentTarget.src = '/404.jpg')}
 						alt="cover art"
-						class="mb-4 aspect-square h-80 min-w-80 object-cover"
+						class="aspect-square w-full bg-red-500 object-cover"
 					/>
 					{#if editing}
-						<input
-							type="text"
-							name="coverArt"
-							bind:value={draft.coverArt}
-							class="w-full rounded-md bg-accent px-4 py-2 font-mono text-xs text-text ring-primary outline-none focus:ring-2"
-						/>
-						<p class="mb-4 text-center font-zcool text-sm">
-							Upload images to <a
-								class="underline"
-								href="https://hackclub.enterprise.slack.com/archives/C016DEDUL87">#cdn</a
-							>
-							or <a class="underline" href="https://cdn.hackclub.com/">cdn.hackclub.com</a>
-						</p>
-					{/if}
-					{#if editing}
-						<select
-							multiple
-							name="hackatimeProjects"
-							class="w-full rounded-md bg-accent p-4 text-center font-gothic font-bold text-text ring-primary focus:ring-2 focus:outline-none"
-						>
-							{#each hackatimeProjects as proj}
-								{#if proj.claimedBy == null}
-									<option selected={proj.claimedBy == data.project!.id} value={proj.name}>
-										{proj.name}
-									</option>
-								{:else}
-									<option disabled value={proj.name}>{proj.name}</option>
-								{/if}
-							{/each}
-						</select>
-						<sub class="mt-2 mb-4 block text-center font-zcool text-text"
-							>Ctrl+Click to select multiple</sub
-						>
-					{:else}
-						<p
-							class="text-center font-gothic text-xl text-primary"
-							title={data.project!.hackatimeProjects!.join(', ')}
-						>
-							{hoursText}
-						</p>
-					{/if}
-					{#if editing}
-						<select
-							name="category"
-							id="category"
-							bind:value={draft.category}
-							class="w-full rounded-md bg-accent p-2 text-center font-gothic text-sm font-bold text-text ring-primary focus:ring-2 focus:outline-none"
-						>
-							<option value="GAME">Game</option>
-							<option value="WEBSITE">Website</option>
-							<option value="DESKTOP_APP">Desktop App</option>
-							<option value="CLI">CLI</option>
-							<option value="OTHER">Other</option>
-						</select>
-					{:else}
-						<p class="text-center font-gothic text-sm text-secondary italic">
-							{data.project?.category.replaceAll('_', ' ')}
-						</p>
-					{/if}
-				</div>
-				<div class="w-md">
-					{#if editing}
-						<textarea
-							bind:value={draft.title}
-							name="title"
-							rows="1"
-							class="mb-4 field-sizing-content w-full resize-none rounded-md bg-accent px-4 py-2 font-nikkyou text-5xl text-primary ring-primary outline-none focus:ring-2"
-						></textarea>
-						<textarea
-							bind:value={draft.description}
-							name="description"
-							class="field-sizing-content min-h-80 w-full rounded-md bg-accent p-4 font-zcool text-text ring-primary outline-none focus:ring-2"
-						></textarea>
-					{:else}
-						<h1 class="mb-4 font-nikkyou text-5xl text-primary">{data.project?.title}</h1>
-						{#if data.project?.description!.trim().length != 0}
-							<p class="w-full font-zcool text-text">{data.project?.description}</p>
-						{:else}
-							<p class="text-center font-zcool text-xl text-secondary">No Description Provided</p>
-						{/if}
+						<div class="absolute bottom-8 left-1/2 w-9/10 -translate-x-1/2">
+							<input
+								type="text"
+								name="coverArt"
+								placeholder="Image URL"
+								bind:value={draft.coverArt}
+								class="w-full rounded-md bg-accent px-4 py-2 font-mono text-xs text-text ring-primary outline-none focus:ring-2"
+							/>
+							<p class="mt-2 rounded-md bg-accent p-2 text-center font-zcool text-sm">
+								Upload images to <a
+									class="underline"
+									href="https://hackclub.enterprise.slack.com/archives/C016DEDUL87">#cdn</a
+								>
+								or <a class="underline" href="https://cdn.hackclub.com/">cdn.hackclub.com</a>
+							</p>
+						</div>
 					{/if}
 				</div>
 			</div>
