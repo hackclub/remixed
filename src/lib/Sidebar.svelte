ff25be86d66ebef49e4612705a53ae914968b90c
Shuflduf <shuflduf@gmail.com>
2026-03-13 11:59:53 -0600

    feat(ui): add confirmation to logging out

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -8,7 +8,7 @@
 		{ name: 'Discover', route: '/discover' },
 		{ name: 'Shop', route: '/shop' },
 		{ name: 'Profile', route: '/user' },
-		{ name: 'Log Out', route: '/auth/logout' }
+		{ name: 'Log Out', route: 'LOGOUT' }
 	];
 
 	let currentPageIndex: number = $state(
@@ -26,25 +26,54 @@
 	function getTextColor(i: number): string {
 		return i == currentPageIndex ? 'var(--color-primary)' : 'var(--color-secondary)';
 	}
+
+	function getStyles(i: number): string {
+		return `top: ${getRecordTop(i)}px;
+			z-index: ${i};
+			background-color: ${getBgColor(i)};
+			color: ${getTextColor(i)};
+			left: ${i == hoveredIndex ? 0 : -64}px;
+			rotate: ${i == hoveredIndex ? 45 : -90}deg;
+		`;
+	}
 </script>
+
+<div
+	class="fixed top-1/2 left-1/2 w-90 -translate-1/2 rounded-md bg-accent p-8 text-text shadow-md"
+	popover
+	id="confirm-signout"
+>
+	<a
+		href="/auth/logout"
+		data-sveltekit-preload-data="off"
+		class="block w-full cursor-pointer rounded-md bg-primary px-4 py-4 text-center font-gothic text-xl text-white"
+	>
+		Log Out
+	</a>
+</div>
 
 <div class="fixed h-screen py-32">
 	{#each records as record, i}
-		<a
-			style="top: {getRecordTop(i)}px;
-				z-index: {i};
-				background-color: {getBgColor(i)};
-				color: {getTextColor(i)};
-				left: {i == hoveredIndex ? 0 : -64}px;
-				rotate: {i == hoveredIndex ? 45 : -90}deg;
-				"
-			href={record.route}
-			data-sveltekit-preload-data={record.route != '/auth/logout' ? '' : 'off'}
-			class="absolute flex size-36 -rotate-90 rounded-full border-2 border-secondary bg-red-500 transition-all duration-200 hover:rotate-45"
-			onmouseenter={() => (hoveredIndex = i)}
-			onmouseleave={() => (hoveredIndex = currentPageIndex)}
-		>
-			<h1 class="mt-6 w-full text-center font-gothic">{record.name}</h1>
-		</a>
+		{#if record.route == 'LOGOUT'}
+			<button
+				popovertarget="confirm-signout"
+				style={getStyles(i)}
+				class="absolute flex size-36 -rotate-90 cursor-pointer rounded-full border-2 border-secondary bg-red-500 transition-all duration-200 hover:rotate-45"
+				onmouseenter={() => (hoveredIndex = i)}
+				onmouseleave={() => (hoveredIndex = currentPageIndex)}
+			>
+				<h1 class="mt-6 w-full text-center font-gothic">{record.name}</h1>
+			</button>
+		{:else}
+			<a
+				style={getStyles(i)}
+				href={record.route}
+				class="absolute flex size-36 -rotate-90 rounded-full border-2 border-secondary bg-red-500 transition-all duration-200 hover:rotate-45"
+				onmouseenter={() => (hoveredIndex = i)}
+				onmouseleave={() => (hoveredIndex = currentPageIndex)}
+			>
+				<h1 class="mt-6 w-full text-center font-gothic">{record.name}</h1>
+			</a>
+		{/if}
 	{/each}
 </div>
