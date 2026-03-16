fb21b1914686d44da70e0a36a418238c26a0ab24
Shuflduf <shuflduf@gmail.com>
2026-03-16 11:01:57 -0600

    fix(ui): cd project text color

--- src/lib/CDProject.svelte
+++ src/lib/CDProject.svelte
@@ -21,7 +21,7 @@
 	<div
 		class="absolute bottom-1/4 left-1/2 w-90 -translate-x-1/2 -rotate-12 bg-primary transition-all group-hover:bottom-0 group-hover:rotate-0"
 	>
-		<h1 class="text-accent mx-auto max-w-80 text-center font-gothic text-3xl">
+		<h1 class="mx-auto max-w-80 text-center font-gothic text-3xl text-text">
 			{project.title}
 		</h1>
 	</div>
