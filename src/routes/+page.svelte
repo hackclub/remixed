29c36a35b2f36860c5747c9aa6f1670ca1d9a35f
Shuflduf <shuflduf@gmail.com>
2026-03-16 20:07:18 -0600

    feat(landing): add the very first elements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,30 @@
 <script lang="ts">
 	import { env } from '$env/dynamic/public';
-	import { records } from '$lib/records';
 
 	let hackatimeOauthUrl =
 		'https://hackatime.hackclub.com/oauth/authorize?' +
 		new URLSearchParams({
 			client_id: env.PUBLIC_HACKATIME_OAUTH_UID!,
 			redirect_uri: env.PUBLIC_CALLBACK_URL!,
-			response_type: 'code'
+			response_type: 'code',
 		}).toString();
-	let selectedIndex: number | null = $state(null);
-	const expandAmount = 300;
-
-	function getRecordTop(i: number) {
-		return i * 60 + (selectedIndex !== null && i > selectedIndex ? expandAmount : 0);
-	}
-
-	function getSleeveTop() {
-		return records.length * 60 + (selectedIndex !== null ? expandAmount : 0);
-	}
 </script>
 
-<a href={hackatimeOauthUrl}>LOGIN</a>
-<div class="relative mt-16 flex w-screen justify-center">
-	{#each records as record, i}
-		<button
-			class="absolute flex h-200 w-200 cursor-pointer flex-col items-center gap-16 rounded-full pt-4 transition-all duration-300"
-			style="top: {getRecordTop(i)}px; z-index: {i}; background-color: {record.theme
-				.bg}; border: 2px solid {record.theme.border};"
-			onclick={() => (selectedIndex = selectedIndex === i ? null : i)}
+<a href={hackatimeOauthUrl} class="fixed">LOGIN</a>
+<div
+	class="relative flex h-screen w-screen items-center justify-center overflow-clip bg-accent-purple"
+>
+	<img src="/landing/concentric.png" alt="ellipse" class="absolute top-0 h-full object-cover" />
+	<div class="relative -top-8 flex -rotate-6 flex-col justify-center">
+		<div class="relative flex min-w-500 justify-center bg-text">
+			<img src="/logo.png" alt="logo" class="h-30" />
+		</div>
+		<h2
+			style="-webkit-text-stroke: 4px white; paint-order: stroke fill;"
+			class=" mt-2 text-center font-jua text-2xl text-text"
 		>
-			<h1 style="color: {record.theme.titleColor}">{record.title}</h1>
-			<p style="color: {record.theme.descColor}">{record.desc}</p>
-		</button>
-	{/each}
-
-	<div
-		class="absolute z-50 h-200 w-200 rounded-b-sm bg-stone-800 text-center text-white transition-all duration-300"
-		style="top: {getSleeveTop()}px"
-	>
-		<h1 class="m-16 text-5xl italic">Remixed</h1>
-		<p>
-			turn your music taste into a website, create a rhythm game, or ship a music related project!!
-			💖
-		</p>
+			ship a rhythm game, get headphones!
+		</h2>
 	</div>
 </div>
+<div class="h-40"></div>
