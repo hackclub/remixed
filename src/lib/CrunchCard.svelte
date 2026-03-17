3220f4b6f9d949ac843726d74192fb716b59140f
Shuflduf <shuflduf@gmail.com>
2026-03-17 09:52:46 -0600

    feat(landing): crunches

---
+++ src/lib/CrunchCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	let { img, h2, h1, class: className } = $props();
+</script>
+
+<div
+	class="{className} relative w-50 rounded-2xl bg-linear-to-br from-secondary to-[#53C1D7] p-1 shadow-xl"
+>
+	<div class="flex flex-col justify-center rounded-xl bg-text p-4">
+		<img src={img} alt="crunch" class="origin-bottom scale-140" />
+		<div class="text-md relative flex justify-center text-center font-jua">
+			<h2 class="text-stroke bg-light">{h2}</h2>
+			<h2 class="text-dark-gradient absolute top-0 p-1">{h2}</h2>
+		</div>
+		<div class="relative flex justify-center text-center font-jua text-2xl">
+			<h1 class="text-stroke bg-light">{h1}</h1>
+			<h1 class="text-dark-gradient absolute top-0 p-1">{h1}</h1>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { env } from '$env/dynamic/public';
+	import CrunchCard from '$lib/CrunchCard.svelte';
 
 	let hackatimeOauthUrl =
 		'https://hackatime.hackclub.com/oauth/authorize?' +
@@ -36,12 +37,9 @@
 		<button
 			class="relative top-0 mx-auto mt-4 w-max cursor-pointer rounded-2xl bg-linear-to-r from-secondary to-[#54C1D7] p-1 shadow-none transition-all hover:-top-1 hover:shadow-lg/30 active:top-1 active:shadow-none"
 		>
-			<div class="rounded-2xl bg-text px-24 py-2">
+			<div class="rounded-xl bg-text px-24 py-2">
 				<div class="relative font-jua text-3xl">
-					<span
-						style="-webkit-text-stroke: 4px transparent;"
-						class=" bg-linear-to-r from-[#6EF5FB] to-[#938BEC] bg-clip-text p-1 text-transparent select-none"
-					>
+					<span class="text-stroke bg-linear-to-r from-[#6EF5FB] to-[#938BEC] p-1">
 						join now!
 					</span>
 					<span
@@ -53,4 +51,59 @@
 		</button>
 	</div>
 </div>
-<div class="h-40"></div>
+<div class="relative h-300">
+	<div class="relative z-2 flex flex-col gap-16">
+		<div class="relative flex justify-center text-center font-jua text-3xl">
+			<h1 class="text-stroke bg-light">Choose a pathway and, get music-related goodies!</h1>
+			<h1 class="text-dark-gradient absolute top-0 p-1">
+				Choose a pathway and, get music-related goodies!
+			</h1>
+		</div>
+		<div class="flex justify-center gap-16 p-16">
+			<CrunchCard
+				img="/landing/crunch_pink.png"
+				h2="PATHWAY #1"
+				h1="Rhythm Game"
+				class="top-4 rotate-6"
+			/>
+			<CrunchCard
+				img="/landing/crunch_green_spotify.png"
+				h2="PATHWAY #2"
+				h1="Audio Editor"
+				class="-top-4 -rotate-6"
+			/>
+			<CrunchCard
+				img="/landing/crunch_pink.png"
+				h2="PATHWAY #3"
+				h1="Music Player"
+				class=" rotate-6"
+			/>
+		</div>
+		<div class="relative flex justify-center text-center font-jua text-3xl">
+			<h1 class="text-stroke bg-light">
+				...or make something completely different, as long as it’s about music!
+			</h1>
+			<h1 class="text-dark-gradient absolute top-0 p-1">
+				...or make something completely different, as long as it’s about music!
+			</h1>
+		</div>
+		<div class="mt-8 flex justify-center">
+			<CrunchCard
+				img="/landing/crunch_green_spotify.png"
+				h2="WILDCARD"
+				h1="Anything else!"
+				class=""
+			/>
+		</div>
+	</div>
+	<div class="absolute -top-40 z-0 w-full">
+		<div
+			class="h-200 w-full mask-[url(/landing/dots-mask-main.png)] mask-contain mask-repeat-x"
+			style="background: repeating-linear-gradient(-70deg, #7dd9d9, #7dd9d9 8px, #4ab5d0 8px, #4ab5d0 18px);"
+		></div>
+		<div
+			style="background: repeating-linear-gradient(-70deg, #7dd9d9, #7dd9d9 8px, #4ab5d0 8px, #4ab5d0 18px);"
+			class="h-200 w-full"
+		></div>
+	</div>
+</div>
