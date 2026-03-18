32746efdb248e6863e9c9a58c42c0fc3ae584d00
Shuflduf <shuflduf@gmail.com>
2026-03-18 00:35:03 -0600

    feat(ui): basic dot background

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -24,20 +24,5 @@
 	});
 </script>
 
-<Sidebar />
-<div class="p-10 pl-40">
-	<div class="mb-6 flex items-center justify-center gap-8">
-		<h1 class="{styleH1} text-text">Projects</h1>
-		<a href="/projects/new" class="{styleButton}  bg-primary">+ New</a>
-	</div>
-
-	{#if userProjects.length > 0}
-		<div class="flex w-full flex-wrap justify-center gap-6">
-			{#each userProjects as project}
-				<CDProject {project} />
-			{/each}
-		</div>
-	{:else}
-		<p class="text-center font-gothic text-xl text-text">Get started by creating a project!</p>
-	{/if}
-</div>
+<img src="/dashboard/dots-tl.png" alt="dots" class="fixed top-0 left-0 w-2/3" />
+<img src="/dashboard/dots-br.png" alt="dots" class="fixed right-0 bottom-0 w-3/5" />
