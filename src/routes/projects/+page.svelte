9b2c59c5dc164894cfd3460329b2276e4abd8e1b
Shuflduf <shuflduf@gmail.com>
2026-03-18 09:55:07 -0600

    feat(ui): make project cards closer to original design and add hover and active states

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -4,6 +4,7 @@
 	import CassetteProject from '$lib/CassetteProject.svelte';
 	import { onMount } from 'svelte';
 	import { styleButton, styleH1 } from '$lib/styles';
+	import CoverArt from '$lib/CoverArt.svelte';
 
 	let userProjects: any[] = $state([]);
 
@@ -30,30 +31,31 @@
 
 <div class="relative z-2 flex h-screen w-screen items-center justify-center">
 	<div class="grid grid-cols-3 gap-16">
-		<div
-			class="flex w-60 flex-col items-center gap-8 rounded-3xl border-4 border-[#8B81FF] bg-text p-8 shadow-simple"
+		<a
+			href="/projects/new"
+			class="hover-effect flex w-70 flex-col items-center gap-8 rounded-3xl border-4 border-[#8B81FF] bg-text p-8"
 		>
 			<img src="/dashboard/plus_fill.png" alt="plus" class="m-4 w-16" />
-			<h2 class="text-center font-jua text-3xl text-[#E2BEFF] text-shadow-lg/30">
+			<h2 class="text-center font-jua text-4xl text-[#E2BEFF] text-shadow-lg/30">
 				Create new project
 			</h2>
-		</div>
+		</a>
 		{#each userProjects as proj}
-			<div
-				class="flex w-60 flex-col rounded-3xl border-4 border-[#8B81FF] bg-text p-4 shadow-simple"
+			<a
+				href="/projects/{proj.id}"
+				class="hover-effect w-70 rounded-3xl border-4 border-[#8B81FF] bg-text p-4"
 			>
-				<img
+				<CoverArt
 					src={proj.coverArt}
-					alt="cover"
-					class="h-30 w-full rounded-xl border-4 border-[#E2BEFF] object-cover px-4"
+					class="mb-4 h-30 w-full rounded-xl border-4 border-[#E2BEFF] object-cover"
 				/>
 				<div class="flex grow flex-col justify-end">
-					<h2 class=" line-clamp-1 font-jua text-4xl text-[#E2BEFF] text-shadow-lg/30">
+					<h2 class=" line-clamp-1 font-jua text-3xl text-[#E2BEFF] text-shadow-lg/30">
 						{proj.title}
 					</h2>
-					<p class="line-clamp-2 font-jua text-[#E2BEFF]">{proj.description}</p>
+					<p class="line-clamp-3 font-jua text-[#E2BEFF]">{proj.description}</p>
 				</div>
-			</div>
+			</a>
 		{/each}
 	</div>
 </div>
