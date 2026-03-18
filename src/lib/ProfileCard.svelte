12680a4135c2b6c755b14e0757521cf50a13d587
Shuflduf <shuflduf@gmail.com>
2026-03-18 13:26:09 -0600

    feat(ui): add profile card and start global layout

---
+++ src/lib/ProfileCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import CoverArt from './CoverArt.svelte';
+
+	let { user } = $props();
+</script>
+
+{#if user}
+	<div class="fixed right-8 bottom-8 z-20">
+		<a
+			href="/user/{user.id}"
+			class=" hover-effect-shadow flex gap-4 rounded-xl border-4 border-[#8B81FF] bg-text p-4 font-jua text-light text-shadow-flat"
+		>
+			<CoverArt src={user.avatarUrl} class="h-12 rounded-full" />
+			<div>
+				<h2 class="text-xl">
+					@{user.username}
+				</h2>
+				<p>{user.notesBalance} notes</p>
+			</div>
+		</a>
+	</div>
+{/if}
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,10 +2,12 @@
 	import Sidebar from '$lib/Sidebar.svelte';
 	import CDProject from '$lib/CDProject.svelte';
 	import CassetteProject from '$lib/CassetteProject.svelte';
+	import ProfileCard from '$lib/ProfileCard.svelte';
 	import { onMount } from 'svelte';
 	import { styleButton, styleH1 } from '$lib/styles';
 	import CoverArt from '$lib/CoverArt.svelte';
 
+	let { data } = $props();
 	let projectView: HTMLElement | null = $state(null);
 	let userProjects: any[] = $state([]);
 
@@ -39,6 +41,7 @@
 <img src="/dashboard/dots-tl.png" alt="dots" class="fixed top-0 left-0 w-2/3" />
 <img src="/dashboard/dots-br.png" alt="dots" class="fixed right-0 bottom-0 w-3/5" />
 <Sidebar />
+<ProfileCard user={data.user} />
 
 <div class="relative z-2 flex h-screen w-screen items-center justify-center">
 	<button class="hover-effect mr-8 cursor-pointer" onclick={() => scroll(false)}>
@@ -53,7 +56,7 @@
 			class="hover-effect-shadow flex w-72 snap-start flex-col items-center gap-8 rounded-3xl border-4 border-[#8B81FF] bg-text p-8"
 		>
 			<img src="/dashboard/plus_fill.png" alt="plus" class="m-4 w-16" />
-			<h2 class="text-center font-jua text-4xl text-[#E2BEFF] text-shadow-lg/30">
+			<h2 class="text-center font-jua text-4xl text-[#E2BEFF] text-shadow-flat">
 				Create new project
 			</h2>
 		</a>
@@ -67,7 +70,7 @@
 					class="mb-4 h-30 w-full rounded-xl border-4 border-[#E2BEFF] object-cover"
 				/>
 				<div class="flex grow flex-col justify-end">
-					<h2 class=" line-clamp-1 font-jua text-3xl text-[#E2BEFF] text-shadow-lg/30">
+					<h2 class=" line-clamp-1 font-jua text-3xl text-[#E2BEFF] text-shadow-flat">
 						{proj.title}
 					</h2>
 					<p class="line-clamp-3 font-jua text-[#E2BEFF]">{proj.description}</p>
