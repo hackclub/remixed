146088669eb2ed72b3bc8c1577fdac7d3d9299da
Shuflduf <shuflduf@gmail.com>
2026-03-18 20:51:44 -0600

    feat(ui): start working on gallery

--- src/routes/(app)/gallery/+page.svelte
+++ src/routes/(app)/gallery/+page.svelte
@@ -1,5 +1,30 @@
 <script lang="ts">
 	import PageHeader from '$lib/PageHeader.svelte';
+	import ProjectCard from '$lib/ProjectCard.svelte';
+	import { onMount } from 'svelte';
+
+	let projectList: any[] = $state([]);
+	let waterfall: HTMLElement | null = $state(null);
+
+	function waterfallScrolled(e: Event) {
+		const elem: HTMLDivElement = e.target as HTMLDivElement;
+		const atBottom = Math.abs(elem.scrollHeight - elem.clientHeight - elem.scrollTop) <= 10;
+		console.log(atBottom);
+	}
+
+	onMount(() => {
+		fetch('api/project_list')
+			.then((resp) => resp.json())
+			.then((resp) => (projectList = resp));
+	});
 </script>
 
 <PageHeader title="Gallery" desc="Look at what other peeps have made!" />
+
+<div onscroll={waterfallScrolled} class="h-screen w-screen overflow-y-auto">
+	{#each projectList as proj}
+		<div>
+			<ProjectCard {proj} />
+		</div>
+	{/each}
+</div>
