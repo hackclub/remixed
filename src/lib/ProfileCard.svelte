493068788f2ad4b2ba50e3294b72e4c608708472
Shuflduf <shuflduf@gmail.com>
2026-03-18 20:42:04 -0600

    fix(ui): make profile card more accurate

--- src/lib/ProfileCard.svelte
+++ src/lib/ProfileCard.svelte
@@ -8,11 +8,11 @@
 	<div class="fixed right-8 bottom-8 z-20">
 		<a
 			href="/user/{user.id}"
-			class=" hover-effect-shadow flex gap-4 rounded-xl border-4 border-[#8B81FF] bg-text p-4 font-jua text-light text-shadow-flat"
+			class="hover-effect-shadow flex gap-4 rounded-xl border-4 border-[#8B81FF] bg-text px-8 py-4 font-jua text-light text-shadow-flat"
 		>
-			<CoverArt src={user.avatarUrl} class="h-12 rounded-full" />
-			<div>
-				<h2 class="text-xl">
+			<CoverArt src={user.avatarUrl} class="h-16 rounded-full" />
+			<div class="flex flex-col justify-center">
+				<h2 class="text-2xl">
 					@{user.username}
 				</h2>
 				<p>{user.notesBalance} notes</p>
