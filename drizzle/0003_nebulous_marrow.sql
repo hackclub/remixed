a91a53204993c3b2b3d8a4f3ffd30ca4bebbf3cb
ascpixi <44982772+ascpixi@users.noreply.github.com>
2026-03-24 14:22:43 -0400

    fix: regenerate Drizzle migrations

--- drizzle/0003_nebulous_marrow.sql
+++ drizzle/0003_nebulous_marrow.sql
@@ -1,6 +1,5 @@
-ALTER TYPE "public"."ship_status" ADD VALUE IF NOT EXISTS 'CANCELLED';--> statement-breakpoint
-ALTER TYPE "public"."audit_category" ADD VALUE IF NOT EXISTS 'PROJECT';--> statement-breakpoint
-
+ALTER TYPE "public"."audit_category" ADD VALUE 'PROJECT';--> statement-breakpoint
+ALTER TYPE "public"."ship_status" ADD VALUE 'CANCELLED';--> statement-breakpoint
 CREATE TABLE "deleted_projects" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"original_id" integer NOT NULL,
@@ -8,7 +7,7 @@ CREATE TABLE "deleted_projects" (
 	"title" text NOT NULL,
 	"description" text,
 	"cover_art_url" text,
-	"category" "public"."category" NOT NULL,
+	"category" "category" NOT NULL,
 	"hackatime_projects" text[] NOT NULL,
 	"hackatime_seconds" integer,
 	"github_url" text,
@@ -17,22 +16,22 @@ CREATE TABLE "deleted_projects" (
 	"deleted_at" timestamp DEFAULT now() NOT NULL,
 	"deleted_by_user_id" integer NOT NULL,
 	CONSTRAINT "deleted_projects_original_id_unique" UNIQUE("original_id")
-);--> statement-breakpoint
-
+);
+--> statement-breakpoint
 CREATE TABLE "deleted_ships" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"original_id" integer NOT NULL,
 	"project_id" integer NOT NULL,
 	"user_id" integer NOT NULL,
 	"seconds" integer NOT NULL,
-	"status" "public"."ship_status" NOT NULL,
+	"status" "ship_status" NOT NULL,
 	"submitted_at" timestamp NOT NULL,
 	"feedback" text,
 	"deleted_at" timestamp DEFAULT now() NOT NULL,
 	"deleted_by_user_id" integer NOT NULL,
 	CONSTRAINT "deleted_ships_original_id_unique" UNIQUE("original_id")
-);--> statement-breakpoint
-
+);
+--> statement-breakpoint
 CREATE TABLE "deleted_shop_items" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"original_id" integer NOT NULL,
@@ -43,8 +42,8 @@ CREATE TABLE "deleted_shop_items" (
 	"deleted_at" timestamp DEFAULT now() NOT NULL,
 	"deleted_by_user_id" integer NOT NULL,
 	CONSTRAINT "deleted_shop_items_original_id_unique" UNIQUE("original_id")
-);--> statement-breakpoint
-
+);
+--> statement-breakpoint
 ALTER TABLE "deleted_projects" ADD CONSTRAINT "deleted_projects_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "deleted_projects" ADD CONSTRAINT "deleted_projects_deleted_by_user_id_users_id_fk" FOREIGN KEY ("deleted_by_user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "deleted_ships" ADD CONSTRAINT "deleted_ships_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
