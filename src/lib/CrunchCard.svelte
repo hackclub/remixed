683e76d1bcf8e85aafcfed61197515b8a20f77b6
ascpixi <44982772+ascpixi@users.noreply.github.com>
2026-03-23 20:25:08 -0400

    fix(landing): maybe fix shop marquee?

--- src/lib/CrunchCard.svelte
+++ src/lib/CrunchCard.svelte
@@ -13,18 +13,22 @@
 
 <div
 	style={styleName}
-	class="{className} relative w-max min-w-50 rounded-2xl bg-linear-to-br from-secondary to-[#53C1D7] p-1 shadow-xl"
+	class="{className} w-max min-w-50 rounded-2xl bg-linear-to-br from-secondary to-[#53C1D7] p-1 shadow-xl"
 >
-	{#if normalizeImage}
-		<div
-			class="pointer-events-none absolute -top-28 left-1/2 flex h-36 w-[78%] -translate-x-1/2 items-end justify-center"
-		>
-			<img src={img} alt="crunch" class="h-full w-full object-contain object-bottom" />
-		</div>
-	{:else}
-		<img src={img} alt="crunch" class="pointer-events-none absolute -top-2/3 left-0 w-full origin-bottom" />
-	{/if}
-	<div class="flex flex-col justify-center rounded-xl bg-text p-4 pt-20">
+	<div class="relative flex flex-col justify-center rounded-xl bg-text p-4 pt-20">
+		{#if normalizeImage}
+			<div
+				class="pointer-events-none absolute -top-28 left-1/2 flex h-36 w-[78%] -translate-x-1/2 items-end justify-center"
+			>
+				<img src={img} alt="crunch" class="h-full w-full object-contain object-bottom" />
+			</div>
+		{:else}
+			<img
+				src={img}
+				alt="crunch"
+				class="pointer-events-none absolute -top-2/3 left-0 w-full origin-bottom"
+			/>
+		{/if}
 		<BoldText class="text-md mx-auto flex! font-jua">{h2}</BoldText>
 		<BoldText class="mx-auto flex! font-jua text-2xl">{h1}</BoldText>
 	</div>
