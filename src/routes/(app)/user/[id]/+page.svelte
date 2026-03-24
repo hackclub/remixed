d3d9dc11174b02229cd9dedfb7d965fcbb0e2caf
Shuflduf <shuflduf@gmail.com>
2026-03-23 18:43:28 -0600

    feat(ui): new user page

--- src/routes/(app)/user/[id]/+page.svelte
+++ src/routes/(app)/user/[id]/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import CDProject from '$lib/CDProject.svelte';
 	import CoverArt from '$lib/CoverArt.svelte';
+	import ProjectCard from '$lib/ProjectCard.svelte';
 	import Sidebar from '$lib/Sidebar.svelte';
-	import { styleButton, styleH1, styleH2 } from '$lib/styles';
+	import { styleButton, styleCard, styleH1, styleH2 } from '$lib/styles';
 
 	let { data } = $props();
 
@@ -15,16 +16,16 @@
 	<title>{data.user.username} - Remixed</title>
 </svelte:head>
 
-<div class="p-10">
+<div class="p-10 pb-40">
 	{#if data.currentUser?.id == data.user.id}
-		<button class="{styleButton} mx-auto block" onclick={copyReferral}>Copy Referral Link</button>
+		<button class="{styleButton} mx-auto mb-4 block" onclick={copyReferral}
+			>Copy Referral Link</button
+		>
 	{/if}
 	<h1 class="{styleH1} mb-4 text-text">{data.user!.username}</h1>
 
-	<div
-		class="relative top-0 mx-auto max-w-80 bg-text text-text shadow-button transition-all hover:-top-1 hover:shadow-button-hover"
-	>
-		<CoverArt src={data.user!.avatarUrl} />
+	<div class="{styleCard} relative top-0 mx-auto max-w-80 p-4">
+		<CoverArt src={data.user!.avatarUrl} class="rounded-2xl" />
 		{#if data.user!.roles.includes('STAFF')}
 			<p class="text-center font-gothic text-xl tracking-wider text-light">Remixed Staff</p>
 		{/if}
@@ -33,8 +34,8 @@
 	<h2 class="{styleH2} mt-12 mb-4 text-text">Projects</h2>
 	{#if data.userProjects.length > 0}
 		<div class="flex h-full flex-row flex-wrap justify-center gap-8">
-			{#each data.userProjects as project}
-				<CDProject {project} />
+			{#each data.userProjects as proj}
+				<ProjectCard {proj} />
 			{/each}
 		</div>
 	{:else}
